15bb55d5c6263c4d7e3567d76e7106a7fef7cb99
Nemanja Djordjevic <nemadjo94@gmail.com>
2020-01-09 09:30:28 +0100

    Set Up Models and Entity Framework + SeedData method

---
+++ LibraryAPI/Services/LibraryDBContext.cs
@@ -0,0 +1,58 @@
+﻿using LibraryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.Services
+{
+    public class LibraryDBContext : DbContext
+    {
+        public LibraryDBContext(DbContextOptions<LibraryDBContext> options) : base(options)
+        {
+            // Applies the db options and creates a db
+            Database.Migrate();
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<BookAuthor> BookAuthors { get; set; }
+        public virtual DbSet<BookCategory> BookCategories { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<Review> Reviews { get; set; }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Reviewer> Reviewers { get; set; }
+
+        // Here we set up our entity relationships and other options
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookCategory>()
+                .HasKey(bc => new { bc.BookId, bc.CategoryId });
+
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(b => b.Book)
+                .WithMany(bc => bc.BookCategories)
+                .HasForeignKey(b => b.BookId);
+
+            modelBuilder.Entity<BookCategory>()
+                .HasOne(c => c.Category)
+                .WithMany(bc => bc.BookCategories)
+                .HasForeignKey(c => c.CategoryId);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(ba => new { ba.BookId, ba.AuthorId });
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(b => b.Book)
+                .WithMany(ba => ba.BookAuthors)
+                .HasForeignKey(b => b.BookId);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(a => a.Author)
+                .WithMany(ba => ba.BookAuthors)
+                .HasForeignKey(a => a.AuthorId);
+        }
+
+    }
+}
---
+++ LibraryAPI/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryAPI.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryAPI
+{
+    public class Startup
+    {
+        // This allows access to appsettings.json 
+        public static IConfiguration _configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add MVC to allow servicing pages
+            services.AddMvc();
+            // Get connection string from appsettings.json
+            var connectionString = _configuration["ConnectionStrings:LibraryDbConnectionString"];
+            // This connects db context to our sql db and allows console commands
+            services.AddDbContext<LibraryDBContext>(c => c.UseSqlServer(connectionString));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, LibraryDBContext context) 
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Seed the initial data
+            //context.SeedDataContext();
+
+            // Add MVC to execution pipeline
+            app.UseMvc();
+        }
+    }
+}
